b7011088aa6ee797120873e647d057c81c1dc420
Alejandro Santari <alejandrosantari@gmail.com>
2020-09-05 23:01:03 -0300

    Adding the location botton. Working

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -3,13 +3,20 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:qrscanner/src/model/scan_model.dart';
 
 class MapPage extends StatelessWidget {
+  final MapController mapController = MapController();
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
         title: Text("Coordenates QR"),
-        actions: [IconButton(icon: Icon(Icons.my_location), onPressed: () {})],
+        actions: [
+          IconButton(
+              icon: Icon(Icons.my_location),
+              onPressed: () {
+                mapController.move(scan.getLatLng(), 15);
+              })
+        ],
       ),
       body: _createFlutterMap(scan),
     );
@@ -17,12 +24,14 @@ class MapPage extends StatelessWidget {
 
   Widget _createFlutterMap(ScanModel scan) {
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(
         center: scan.getLatLng(),
         zoom: 10,
       ),
       layers: [
         _createMap(),
+        _createMarks(scan),
       ],
     );
   }
@@ -36,5 +45,22 @@ class MapPage extends StatelessWidget {
               'pk.eyJ1Ijoiam9yZ2VncmVnb3J5IiwiYSI6ImNrODk5aXE5cjA0c2wzZ3BjcTA0NGs3YjcifQ.H9LcQyP_-G9sxhaT5YbVow',
           'id': 'mapbox.outdoors' //street, dark, light, outdoors, satellite,
         });
+  }
+
+  _createMarks(scan) {
+    return MarkerLayerOptions(markers: <Marker>[
+      Marker(
+        width: 100.0,
+        height: 100.0,
+        point: scan.getLatLng(),
+        builder: (context) => Container(
+          child: Icon(
+            Icons.location_on,
+            size: 70.0,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      )
+    ]);
   }
 }
